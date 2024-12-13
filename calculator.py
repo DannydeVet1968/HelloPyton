f5586b0b8dfa3ec1112fbe904b260c0f5d68adea
Danny de Vet <danny.de.vet@telenet.be>
2024-12-13 19:34:46 +0100

    print hello world

---
+++ calculator.py
@@ -0,0 +1,20 @@
+def sum (a,b):
+    result = a + b
+    return result
+
+def substract (a,b):
+    result = a - b
+    return result
+
+def multiply (a,b):
+    result = a * b
+    return result
+
+def divide (a,b):
+    result = a/b
+    return result
+
+def pow (a,b):
+    result = a**b
+    return result
+
---
+++ leeftijd.py
@@ -0,0 +1,7 @@
+leeftijd  = int(input("How old are you?"))
+if (leeftijd >=18):
+                 print("Ge kunt zuipen")
+elif (leeftijd >= 16):
+    print("alleen bier")
+else:
+    print("Te jong!")
